0e6fe77b5319a5eaf272aa7b053d069f480b73de
AvisKk <112995329+Anl0k@users.noreply.github.com>
2023-08-05 13:56:22 +0800

    updated gitignore

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>Hello from React</div>
+        <div>Hello from React </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
